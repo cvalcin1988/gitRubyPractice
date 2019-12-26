c1ead697571cfb05c0c33c9a729d5868eba8271f
Calherbe Valcin <cvalcin1988@gmail.com>
2019-12-26 13:05:44 -0500

    did methods, up to if statments

--- App.rb
+++ App.rb
@@ -65,27 +65,72 @@ puts Math.log(0) #outputs -Infinity
 # Arrays - 13:14
 lucky_numbers = [4,8, "fifteen", 16, 23, 42.0]
 # => indexes     0 1      2      3   4    5
+# => -indexes   -6 -5    -4     -3  -2   -1
 lucky_numbers[0] = 90
 puts lucky_numbers[0]
 puts lucky_numbers[1]
 puts lucky_numbers[-1] #use negative index, can grab in reverse order, 42.0 ~ -1, 23 ~ -2, 16 ~ -3
 puts lucky_numbers[-2]
-puts "\n\n"
+puts "\n\n" #new line, new line
 puts lucky_numbers[2,3] # grab from first index, and continue for seconds length
 puts "\n\n"
-puts lucky_numbers[2,4]
+puts lucky_numbers[2..4] # out index 2 through 4
 puts "\n\n"
 
 puts lucky_numbers.length
 
+puts "\n\n"
 
+# 2d Arrays - 15:01 ~N Dimensional Arrays (2,3,4, etc dimensions)
 
-# 2d Arrays - 15:01 ~N Dimensional Arrays
-
-
+number_grid = [[1, 2], [3, 4]]
+number_grid[0][0] = 99
+number_grid[0][1] = 3
+number_grid[1][0] = 77
+number_grid[1][1] = 4
+#currently number_grid = [[99, 3],[77,4]]
+puts number_grid[0][0]
+puts number_grid[0][1]
+puts number_grid[1][0]
+puts number_grid[1][1]
+puts "\n"
 
 # Array Functions - 15:33
+
+friends = [] #create array
+friends.push("Oscar")
+friends.push("Angela")
+friends.push("Kevin")
+# Add Oscar, Angela, and Kevin to the friends array
+puts "\n"
+friends.pop # removes last item in array
+puts friends # output friends array, then new line
+# Oscar
+# Angela
+# Kevin
+puts "\n" # new line
+
+puts friends.reverse #out friends array in reverse
+# Kevin
+# Angela
+# Oscar
+puts "\n"
+
+puts friends.sort #sorts alphabetically, numerically etc. acts unpredicatably with mixed data type arrays
+puts "\n"
+puts friends.include? "Oscar" # Does friend include string element Oscar, true or false
+
+
 # Methods - 16:58
+
+def add_numbers(num1, num2=99) #num2=99~ Ruby allows default parameters.
+    #when the function run runs, num2 will equal 99 unless a new argument is sent
+    #check the output of the sum example below for verification
+    return num1 + num2
+end
+
+sum = add_numbers(4,3)
+puts sum
 # If Statements - 18:08
 # Switch Statements - 19:53
 # Dictionaries - 20:43
